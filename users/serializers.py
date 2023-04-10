62c985d046ea1b3e8b4c3c18f2f450b7981a2d6a
Varun <varunr.nair@outlook.com>
2023-04-10 19:01:04 +0530

    added user api

---
+++ users/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from users.models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['firstname', 'email']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(max_length=32,min_length=8,write_only = True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    password=serializers.CharField(max_length=32,min_length=8,write_only = True)
+    
+    class Meta:
+        model  = CustomUser
+        fields = ['email','password']
--- users/views.py
+++ users/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers import *
+from .models import *
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({'status': 'success', 'user_id': user.id})
+        else:
+            return Response(serializer.errors, status=400)
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return Response({'status': 'success'})
+            else:
+                return Response({'status': 'failed', 'message': 'Invalid credentials'}, status=401)
+        else:
+            return Response(serializer.errors, status=400)
+        
+class display(APIView):
+    serializer_class=UserSerializer
+    def get(self,request):
+        queryset = CustomUser.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+        
